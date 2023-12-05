407ad5707db63101976f6c589533f7ab1b3489d1
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2023-12-05 08:55:24 +0100

    Ajoutez des fichiers projet.

---
+++ MYSQL - ADO/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace MYSQL___ADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_ListeAuteurs = new System.Windows.Forms.DataGridView();
+            this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prenom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Afficher = new System.Windows.Forms.Button();
+            this.btn_modifier = new System.Windows.Forms.Button();
+            this.btn_supprimer = new System.Windows.Forms.Button();
+            this.btn_nouveau = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ListeAuteurs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_ListeAuteurs
+            // 
+            this.dgv_ListeAuteurs.AllowUserToAddRows = false;
+            this.dgv_ListeAuteurs.AllowUserToDeleteRows = false;
+            this.dgv_ListeAuteurs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ListeAuteurs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Num,
+            this.Nom,
+            this.Prenom,
+            this.Nation});
+            this.dgv_ListeAuteurs.Location = new System.Drawing.Point(53, 22);
+            this.dgv_ListeAuteurs.Name = "dgv_ListeAuteurs";
+            this.dgv_ListeAuteurs.ReadOnly = true;
+            this.dgv_ListeAuteurs.RowHeadersWidth = 51;
+            this.dgv_ListeAuteurs.RowTemplate.Height = 24;
+            this.dgv_ListeAuteurs.Size = new System.Drawing.Size(553, 486);
+            this.dgv_ListeAuteurs.TabIndex = 0;
+            this.dgv_ListeAuteurs.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Num
+            // 
+            this.Num.HeaderText = "Numéro";
+            this.Num.MinimumWidth = 6;
+            this.Num.Name = "Num";
+            this.Num.ReadOnly = true;
+            this.Num.Width = 125;
+            // 
+            // Nom
+            // 
+            this.Nom.HeaderText = "Nom";
+            this.Nom.MinimumWidth = 6;
+            this.Nom.Name = "Nom";
+            this.Nom.ReadOnly = true;
+            this.Nom.Width = 125;
+            // 
+            // Prenom
+            // 
+            this.Prenom.HeaderText = "Prénom";
+            this.Prenom.MinimumWidth = 6;
+            this.Prenom.Name = "Prenom";
+            this.Prenom.ReadOnly = true;
+            this.Prenom.Width = 125;
+            // 
+            // Nation
+            // 
+            this.Nation.HeaderText = "Nationalité";
+            this.Nation.MinimumWidth = 6;
+            this.Nation.Name = "Nation";
+            this.Nation.ReadOnly = true;
+            this.Nation.Width = 125;
+            // 
+            // btn_Afficher
+            // 
+            this.btn_Afficher.Location = new System.Drawing.Point(689, 37);
+            this.btn_Afficher.Name = "btn_Afficher";
+            this.btn_Afficher.Size = new System.Drawing.Size(75, 23);
+            this.btn_Afficher.TabIndex = 1;
+            this.btn_Afficher.Text = "Afficher";
+            this.btn_Afficher.UseVisualStyleBackColor = true;
+            this.btn_Afficher.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_modifier
+            // 
+            this.btn_modifier.Location = new System.Drawing.Point(689, 108);
+            this.btn_modifier.Name = "btn_modifier";
+            this.btn_modifier.Size = new System.Drawing.Size(75, 23);
+            this.btn_modifier.TabIndex = 2;
+            this.btn_modifier.Text = "Modifier";
+            this.btn_modifier.UseVisualStyleBackColor = true;
+            this.btn_modifier.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btn_supprimer
+            // 
+            this.btn_supprimer.Location = new System.Drawing.Point(689, 170);
+            this.btn_supprimer.Name = "btn_supprimer";
+            this.btn_supprimer.Size = new System.Drawing.Size(75, 23);
+            this.btn_supprimer.TabIndex = 3;
+            this.btn_supprimer.Text = "Supprimer";
+            this.btn_supprimer.UseVisualStyleBackColor = true;
+            // 
+            // btn_nouveau
+            // 
+            this.btn_nouveau.Location = new System.Drawing.Point(689, 233);
+            this.btn_nouveau.Name = "btn_nouveau";
+            this.btn_nouveau.Size = new System.Drawing.Size(75, 23);
+            this.btn_nouveau.TabIndex = 4;
+            this.btn_nouveau.Text = "Nouveau";
+            this.btn_nouveau.UseVisualStyleBackColor = true;
+            this.btn_nouveau.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1490, 537);
+            this.Controls.Add(this.btn_nouveau);
+            this.Controls.Add(this.btn_supprimer);
+            this.Controls.Add(this.btn_modifier);
+            this.Controls.Add(this.btn_Afficher);
+            this.Controls.Add(this.dgv_ListeAuteurs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ListeAuteurs)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_ListeAuteurs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prenom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nation;
+        private System.Windows.Forms.Button btn_Afficher;
+        private System.Windows.Forms.Button btn_modifier;
+        private System.Windows.Forms.Button btn_supprimer;
+        private System.Windows.Forms.Button btn_nouveau;
+    }
+}
+
---
+++ MYSQL - ADO/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace MYSQL___ADO
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "server=btssio.dedyn.io;port=3306;Database=NJOBOJO_Bibli;Uid=NJOBOJO;password=njobojo;";
+        private MySqlConnection maConnection;
+        private MySqlCommand maRequete;
+        private MySqlDataReader monReader;
+
+        public Form1()
+        {
+            InitializeComponent();
+            RemplirListe();
+
+        }
+
+        private void RemplirListe()
+        {
+            try 
+            {
+                maConnection = new MySqlConnection(connectionString);
+                maConnection.Open();
+                maRequete = maConnection.CreateCommand();
+                maRequete.CommandText = "select * from auteur order by nom";
+                monReader = maRequete.ExecuteReader();
+                while (monReader.Read()) {
+
+                    dgv_ListeAuteurs.Rows.Add(
+                            monReader["num"].ToString(),
+                            monReader["nom"].ToString(),
+                            monReader["prenom"].ToString(),
+                            monReader["nationalite"].ToString()
+
+                        );
+                
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("erreur :" + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
